e1e2d52d0ad9342651299772fac5aad960beaf86
Ayushkumar025 <ayushkumar0117@gmail.com>
2025-01-18 21:34:58 +0530

    fifth commit

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className='bg-[#f7f7f7] text-gray-500 h-10 p-2'>
+            <div className='flex gap-6'>
+                <div><span>Terms and Conditions</span></div>
+                <div><span>Privacy Policy</span></div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
